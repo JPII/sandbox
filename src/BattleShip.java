c1d93b223ad93d05ec6d413c12d384a1d10d1212
Smeagle42 <zmathewson@jpiicardinals.org>
2012-11-09 08:34:13 -0600

    new sandbox

---
+++ src/BattleShip.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import javax.swing.*;
+
+
+@SuppressWarnings("serial")
+public class BattleShip extends JFrame
+{
+	public BattleShip(){
+		
+	}
+	public static void main(String[] args){
+		BattleShip battleship = new BattleShip();
+		
+		battleship.setSize(800,600);
+		battleship.setVisible(true);
+	}
+	
+	public void paint(Graphics g){
+		
+	}
+}
