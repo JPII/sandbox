e1483ba334d14399bb86f5d9e18a288fc0e9b724
Smeagle42 <zmathewson@jpiicardinals.org>
2012-12-12 13:30:47 -0600

    cleaned up ImageStorage

--- src/ImageStorage.java
+++ src/ImageStorage.java
@@ -6,22 +6,22 @@ import javax.imageio.*;
 public class ImageStorage 
 {
 	public Image AircraftCarrier;
-	public Image BattleShip,BattleShipN,BattleShipE,BattleShipS;
+	public Image BattleShip;
 	public Image Submarine;
-	public Image Missile;
-	public Image Bullet;
 	public Image Jet;
 	String s;
 
 	public ImageStorage()
 	{
 		try{
-			AircraftCarrier = ImageIO.read(ImageStorage.class.getResource("/com/jpii/navalbattle/res/drawable-game/AircraftCarrier/AircraftCarrier.png"));
-			BattleShip = ImageIO.read(ImageStorage.class.getResource("/com/jpii/navalbattle/res/drawable-game/BattleShip/BattleShip.png"));
-			Bullet = ImageIO.read(ImageStorage.class.getResource("/com/jpii/navalbattle/res/drawable-game/Other/Bullet.png"));
-			Missile = ImageIO.read(ImageStorage.class.getResource("/com/jpii/navalbattle/res/drawable-game/Other/Missle.png"));
-			Jet = ImageIO.read(ImageStorage.class.getResource("/com/jpii/navalbattle/res/drawable-game/Plane/Jet Fighter.png"));
-			Submarine = ImageIO.read(ImageStorage.class.getResource("/com/jpii/navalbattle/res/drawable-game/Submarine/Submarine.png"));
+			s = "AircraftCarrier/AircraftCarrier.png";
+				AircraftCarrier = ImageIO.read(ImageStorage.class.getResource("/com/jpii/navalbattle/res/drawable-game/" + s));
+			s = "BattleShip/BattleShip.png";
+				BattleShip = ImageIO.read(ImageStorage.class.getResource("/com/jpii/navalbattle/res/drawable-game/" + s));
+			s = "Plane/Jet Fighter.png";
+				Jet = ImageIO.read(ImageStorage.class.getResource("/com/jpii/navalbattle/res/drawable-game/" + s));
+			s = "Submarine/Submarine.png";
+				Submarine = ImageIO.read(ImageStorage.class.getResource("/com/jpii/navalbattle/res/drawable-game/" + s));
 		}
 		catch(Exception e){e.printStackTrace();}
 	}
@@ -31,20 +31,11 @@ public class ImageStorage
 	}
 
 	public void drawBattleShip(Graphics2D g, AffineTransform at){
-		//g.drawImage(BattleShip,x,y,null);
 		g.drawImage(BattleShip, at, null);
 	}
 
 	public void drawSubmarine(Graphics g, int x, int y){
 		g.drawImage(Submarine,x,y,null);
-	}
-
-	public void drawMissile(Graphics g, int x, int y){
-		g.drawImage(Missile,x,y,null);
-	}
-
-	public void drawBullet(Graphics g, int x, int y){
-		g.drawImage(Bullet,x,y,null);
 	}
 
 	public void drawJet(Graphics g, int x, int y){
