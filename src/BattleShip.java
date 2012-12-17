1bfcfd7a730b178f51b55ee1323aa703068e3656
abauer <abauer@jpiicardinals.org>
2012-12-17 17:01:32 -0600

    Revert "got bored, made a tank"
    This reverts commit 0892201296270fd793aa2fa4b98589382deb5fdf.

--- src/BattleShip.java
+++ src/BattleShip.java
@@ -2,15 +2,15 @@ import java.awt.*;
 import java.util.*;
 
 public class BattleShip extends Ship {
-	protected ArrayList<Turret> guns;
-	protected ImageStorage is;
+	private ArrayList<Turret> guns;
+	private ImageStorage is;
 	public BattleShip(int x,int y,int color,double angle){
 		super(x,y,color,angle);
 		guns = new ArrayList<Turret>();
 		is = new ImageStorage();
 		initArray();
 	}	
-	protected void initArray(){
+	private void initArray(){
 		guns.add(new Turret(x+70,y+25,270,0,size,WEST));
 		guns.add(new Turret(x+35,y+25,270,0,size,WEST));
 		guns.add(new Turret(x+145,y+25,270,0,size,WEST));
--- src/ImageStorage.java
+++ src/ImageStorage.java
@@ -8,7 +8,6 @@ public class ImageStorage
 	public Image AircraftCarrier;
 	public Image BattleShip;
 	public Image Submarine;
-	public Image Tank;
 	String s;
 
 	public ImageStorage()
@@ -20,8 +19,6 @@ public class ImageStorage
 				BattleShip = ImageIO.read(ImageStorage.class.getResource("/com/jpii/navalbattle/res/drawable-game/" + s));
 			s = "Submarine/Submarine.png";
 				Submarine = ImageIO.read(ImageStorage.class.getResource("/com/jpii/navalbattle/res/drawable-game/" + s));
-			s = "Other/TankBase.png";
-				Tank = ImageIO.read(ImageStorage.class.getResource("/com/jpii/navalbattle/res/drawable-game/" + s));
 		}
 		catch(Exception e){e.printStackTrace();}
 	}
@@ -36,9 +33,5 @@ public class ImageStorage
 
 	public void drawSubmarine(Graphics2D g, AffineTransform at){
 		g.drawImage(Submarine,at,null);
-	}
-	
-	public Image getTank(){
-		return Tank;
 	}
 }
