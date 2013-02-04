6c216082a8dec8e736a089ce54889610ad62a57a
Mr. Pyram <pyram3d@gmail.com>
2013-02-04 12:40:44 -0600

    Syncoronzied network test.

---
+++ src/com/com/com/com/com/com/com/com/thiis/is/maxs/only/$$$$$$$$$$$$$$$$$$$$$.java
@@ -0,0 +1,14 @@
+package com.com.com.com.com.com.com.com.thiis.is.maxs.only;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class $$$$$$$$$$$$$$$$$$$$$ extends Applet {
+	private static final long serialVersionUID = 6430999995371909268L;
+	public void init() {
+		
+	}
+	public void paint(Graphics g) {
+		
+	}
+}
