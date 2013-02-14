f3602e205a72baa29410564480de802c303d2e72
Smeagle42 <zmathewson@jpiicardinals.org>
2013-02-14 13:09:16 -0600

    Started HUD slate

--- src/Bullet.java
+++
@@ -1,8 +0,0 @@
-
-public class Bullet extends BaseBullet{
-	
-	public Bullet(int sx, int sy, int clickx, int clicky, BaseTank fired) {
-		super(sx, sy, clickx, clicky, fired);
-		numBounces = 1;
-	}
-}
