7a9dd516fd10926d3c560eca548f6e8f57657847
Smeagle42 <zmathewson@jpiicardinals.org>
2013-02-14 18:09:34 -0600

    started on buttons

---
+++ src/HUD/Exec.java
@@ -0,0 +1,40 @@
+package HUD;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.imageio.*;
+
+@SuppressWarnings("serial")
+public class Exec extends JFrame
+{	
+	Image button,attack,diplomacy,move;
+	
+	public Exec(){
+		try{
+			button 		= ImageIO.read(Exec.class.getResource("staticButton.png"));
+			attack 		= ImageIO.read(Exec.class.getResource("Attack.png"));
+			diplomacy	= ImageIO.read(Exec.class.getResource("Diplomacy.png"));
+			move		= ImageIO.read(Exec.class.getResource("Move.png"));
+		}
+		catch(Exception e){e.printStackTrace();}
+	}
+	
+	public void paint(Graphics g){
+		for(int x = 100; x <= 180; x += 40)
+			g.drawImage(button,x,100,null);
+		g.drawImage(attack,100,100,null);
+		g.drawImage(diplomacy,100,140,null);
+		g.drawImage(move,100,180,null);
+	}
+	
+	public static void main(String[] args){
+		Exec battleship = new Exec();
+		battleship.setSize(800,600);
+		battleship.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		battleship.setVisible(true);
+	}
+	
+	public void update(Graphics g) {
+		paint(g);
+	}	
+}
