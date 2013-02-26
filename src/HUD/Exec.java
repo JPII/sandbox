7965797e92a88329a4587a1257b92c8fa0574ff4
Smeagle42 <zmathewson@jpiicardinals.org>
2013-02-26 10:05:05 -0600

    finished up HUD background and borders
    Ready to be implemented into the program

--- src/HUD/Exec.java
+++ src/HUD/Exec.java
@@ -45,6 +45,26 @@ public class Exec extends JFrame
 				g.setColor(new Color(gr,b,b));
 				g.fillRect(x,y,4,4);
 			}
+		for(int x0 = w-(((w/3)+(w/3))-8); x0 <= w-((w/3)-8)+5; x0++)
+			for(int y0 = h-125; y0 <= h; y0++)
+			{
+				int r = random(0,250);
+				g.setColor(new Color(r,r,0));
+				g.fillRect(x0,y0,1,1);
+			}
+		g.setColor(new Color(110,110,110));
+		g.fillRect(w-(((w/3)+(w/3))-8),h-125,5,125);
+		g.fillRect(w-((w/3)-8)+5,h-125,5,125);
+		g.setColor(new Color(170,170,170));
+		g.drawLine(w-(w-8),h-125,w,h-125);
+		g.setColor(new Color(155,155,155));
+		g.drawLine(w-(w-8),h-124,w,h-124);
+		g.setColor(new Color(140,140,140));
+		g.drawLine(w-(w-8),h-123,w,h-123);
+		g.setColor(new Color(125,125,125));
+		g.drawLine(w-(w-8),h-122,w,h-122);
+		g.setColor(new Color(110,110,110));
+		g.drawLine(w-(w-8),h-121,w,h-121);
 	}
 	
 	public static int random(int min, int max){
