414cd92dcd760e588e301d8e9e004485239be84d
abauer <abauer@jpiicardinals.org>
2014-03-27 15:41:15 -0500

    fixed it!

--- src/Bird.java
+++ src/Bird.java
@@ -4,13 +4,39 @@ import java.awt.image.BufferedImage;
 
 public class Bird extends BaseTank{
 	
+	private double centerx;
+	private double centery;
+	private int rotation;
+	private int shipRotation;
+	
 	public Bird(int x, int y, double angle) {
 		super(x, y, angle);
+		centerx = x;
+		centery = y;
+		rotation = 0;
+	}
+	
+	public void move(int x, int y){
+		super.move(x, y);
+		centerx+=x;
+		centery+=y;
 	}
 	
 	public void MouseMoved(int x, int y){
-		angle+=2;
+		rotation = getAngle(x,y);
 	}
+	
+	private int getAngle(int x, int y){
+		double ycomp = centery-y;
+    	double xcomp = centerx-x;
+    	double theta = Math.toDegrees((Math.atan(ycomp/xcomp)));
+    	theta+=shipRotation;
+    	if(xcomp<0)
+    		theta+=180;
+    	theta-=27;
+    	return (int) theta;
+	}
+	
 	public void MouseClicked(int x, int y){ }
 
 	protected void base(Graphics g)
@@ -23,7 +49,7 @@ public class Bird extends BaseTank{
 		tank = img;
 		Graphics g2 = tank.getGraphics();
 		g2.drawImage(ImageStorage.getBird(), 0, 0, null);
-		
-		ImageStorage.drawTank(g,tank,rotate(angle,x,y,imgwidth/2,imgheight/2));
+	
+		ImageStorage.drawTank(g,tank,rotate(rotation,x-imgwidth/2,y-imgheight/2,imgwidth/2,imgheight/2));
 	}
 }
